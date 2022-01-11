176fa8e6c52bb19b43733bc1e30a36e04b64a627
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-01-11 15:59:49 +0700

    update cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cookieSession({
     name: 'access-token',
     keys: [process.env.COOKIE_KEY],
     maxAge: 24 * 60 * 60 * 1000,
-    httpOnly: true
+    httpOnly: false
 }))
 
 app.use(passport.initialize())
