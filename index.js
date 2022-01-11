b66ac866092461e9d381aed081d3f5940850d8f4
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-01-11 14:59:57 +0700

    update cors

--- index.js
+++ index.js
@@ -19,16 +19,10 @@ const app = express()
 app.set('trust proxy', 1)
 
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,set-cookie');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
- 
     // Pass to next layer of middleware
     next();
   });
@@ -46,12 +40,12 @@ app.use(cookieSession({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(cors({
-    origin: 'https://infocard-70df1.web.app',
-    // methods: 'GET, POST, PUT, DELETE',
-    credentials: true,
-    exposedHeaders: ['Set-cookie']
-}))
+// app.use(cors({
+//     // origin: 'https://infocard-70df1.web.app',
+//     // methods: 'GET, POST, PUT, DELETE',
+//     credentials: true,
+//     exposedHeaders: ['Set-cookie']
+// }))
 
 app.use('/auth', authRouter)
 app.use('/profile', profileRouter)
