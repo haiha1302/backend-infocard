d23674b8ce18e9c3738d11917ca588ee32739743
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-01-11 17:27:27 +0700

    update cors

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(cors({
-    origin: 'https://infocard-70df1.web.app',
-    // origin: 'http://localhost:3000',
+    // origin: 'https://infocard-70df1.web.app',
+    origin: '*',
     methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
     credentials: true,
     exposedHeaders: ['Set-cookie']
