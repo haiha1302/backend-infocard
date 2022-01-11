fc63bee6cb9f7c7ab5ec92690285663e42e2bcc2
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-01-11 15:57:31 +0700

    update cors

--- router/auth.js
+++ router/auth.js
@@ -55,7 +55,7 @@ router.get('/login/success', validateToken, (req, res) => {
 })
 
 router.get('/login/oauth', (req, res) => {
-    console.log(req.cookies);
+    console.log(req);
     if (req.user) {
         res.status(200).json({
             success: true,
